02763d2a857979bb1ee001988c60773e5f002710
mobs23 <164636987+mobs23@users.noreply.github.com>
2024-03-25 07:04:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,10 +23,10 @@ global.APIs = {
 // Other
 global.apikey = 'bf2d2cf29b3edc604b447983';
 global.ownername = '𝑆𝛪𝐷 𝐵𝛨𝛥𝛪';
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.owner = ['237687535035'];
+global.premium = ['237687535035'];
 global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
-global.author = '𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫';
+global.author = 'OLI𝚩𝚯𝚻𝐖𝚫';
 global.sessionName = 'session';
 global.prefa = ['!', '.'];
 global.sp = '✪';
